4f74022d5dc78d6ef7be3459e96467b281fc2b20
Robert Bystrzanowski <robert.bystrzanowski.sp@lhsystems.com>
2025-09-22 09:23:40 +0200

    initial commit

---
+++ src/test/java/com/example/scoreboardtask/ScoreboardTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scoreboardtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoreboardTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
